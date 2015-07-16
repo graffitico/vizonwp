d6f2f44c4571e3ae799499a6ef99dd4ccf209ddc
Sushant Sudarshan <sushant.sudarshan@myntra.com>
2015-07-17 00:18:19 +0530

    Retrying modifying page.php

--- wp-content/themes/wpex-teris-child/page.php
+++ wp-content/themes/wpex-teris-child/page.php
@@ -13,28 +13,15 @@ get_header(); // Loads the header.php template
 
 if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<?php
-// Show featured image
-if( has_post_thumbnail( get_the_ID() ) ) {
-	$wpex_header_img = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full-size');
-	echo '<div id="page-featured-img"><img src="'. $wpex_header_img[0] .'" alt="'. get_the_title() .'" /></div>';
-} ?>
-
-<div id="page-heading">
-	<h2>One extra line</h2>
-	<h1><?php the_title(); ?></h1>
-</div><!-- /page-heading -->
 
 <div id="single-page-content" class="container sidebar-bg clearfix">
     <article id="post" class="clearfix">
         <div class="entry clearfix">
             <?php the_content(); ?>
         </div><!-- /entry -->
-    <?php comments_template(); ?>
     </article><!-- /post -->
 	<?php
-	endwhile; endif;
-	get_sidebar(); // Loads the sidebar.php file ?>
+	endwhile; endif; ?>
 </div><!-- /container -->
 
 <?php
