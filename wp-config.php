9f177859aafd8b37c6b5a5f97b5a7a8619a0b755
shazadmaved <shazad26@gmail.com>
2015-07-19 13:36:15 +0530

    Intergalactic theme extended instead of tetris now and here the header is not integrated well

--- wp-config.php
+++ wp-config.php
@@ -13,26 +13,19 @@
  *
  * @package WordPress
  */
-
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'sushant');
-
+define('DB_NAME', 'vizury');
 /** MySQL database username */
-define('DB_USER', 'adminpXsiYuE');
-
+define('DB_USER', 'root');
 /** MySQL database password */
-define('DB_PASSWORD', '_DsVJ2WwEdZl');
-
+define('DB_PASSWORD', 'shazad');
 /** MySQL hostname */
-define('DB_HOST', getenv(OPENSHIFT_MYSQL_DB_HOST) . ':' . getenv(OPENSHIFT_MYSQL_DB_PORT));
-
+define('DB_HOST', 'localhost');
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
+define('DB_CHARSET', 'utf8mb4');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
-
 /**#@+
  * Authentication Unique Keys and Salts.
  *
@@ -42,17 +35,15 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
-
+define('AUTH_KEY',         '?|kzb:5F2v%tL^-&V4Fsns(,Bn/]L&nY:lPerE|xFY&u4~]4U^YNb-U;d,B;V42G');
+define('SECURE_AUTH_KEY',  '9K->8EQ/@yAs%K4gv(F?qFh-X-KQOj$m11%s*H?)f3ui!&?CXU68/O,M@*{`6RR!');
+define('LOGGED_IN_KEY',    'u?+7uDQc6MGU)La(ub-U|yn|;HQaiX+Rfo36Jh[jJ%GkkMw3@lk}beW$+J*9|/E-');
+define('NONCE_KEY',        '%wmPFgup3u.K(.fZ$gWwMyJGNB#AvtWydpB-UD[9~E*r^ho8C4r(xm<dpuBgQILV');
+define('AUTH_SALT',        '^ !>6^@}lF$:[Y*l1ob$H&oS4vT:*~fv)d|X8j}7N|i%)j0-7)9@7yovy3SPkp*o');
+define('SECURE_AUTH_SALT', ')-{JedT}Au<egXsJ5Dhx|kz!:#Z=1==1|T@-g{U4u3+v|*P-[9];&Js #FM%U])R');
+define('LOGGED_IN_SALT',   'XeW*zPg--{0ZWQz=J?A+Te[O)Xk^QJv63pnl9RQB8j,dljh_g/Z;kI<ZMm-jXCK9');
+define('NONCE_SALT',       '}:wSfYJpido_fRW}AnK6iE1^2~kb06`hqD3%Ex2kkL3:x!PX^+x32-M`4 jhff;(');
 /**#@-*/
-
 /**
  * WordPress Database Table prefix.
  *
@@ -60,7 +51,6 @@ define('NONCE_SALT',       'put your unique phrase here');
  * prefix. Only numbers, letters, and underscores please!
  */
 $table_prefix  = 'wp_';
-
 /**
  * For developers: WordPress debugging mode.
  *
@@ -69,12 +59,9 @@ $table_prefix  = 'wp_';
  * in their development environments.
  */
 define('WP_DEBUG', false);
-
 /* That's all, stop editing! Happy blogging. */
-
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
-
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
