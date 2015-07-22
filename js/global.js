b9308c1a37f00bf3f262b7234d22fef2913dc56f
shazadmaved <shazad26@gmail.com>
2015-07-22 12:34:04 +0530

    about us page

--- js/global.js
+++ js/global.js
@@ -111,4 +111,23 @@ jQuery(function () {
    		$(this).removeClass('expanded');
    	})
 
+
+// about us js
+
+var winHeight = $(window).innerHeight();
+$(document).ready(function () {
+    $(".ab-panel").height(winHeight);
+    $("body").height(winHeight*$(".ab-panel").length);
+});
+
+window.addEventListener('resize', function (event) {
+    $(".ab-panel").height($(window).innerHeight());
+});
+$(window).on('scroll',function(){
+    $(".ab-panelCon").css('bottom',$(window).scrollTop()*-1);
+});
+
+
+
+
 });
