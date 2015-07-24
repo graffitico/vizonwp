f30784b014012d1a2c5117e571211a8d495828a3
shazadmaved <shazad26@gmail.com>
2015-07-24 18:07:56 +0530

    case studies

--- wp-content/themes/intergalactic-child/page.php
+++ wp-content/themes/intergalactic-child/page.php
@@ -9,7 +9,49 @@
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
+
 get_header(); // Loads the header.php template
+
+
+if ($post->post_title == 'case studies') { 
+  $case_posts =  new WP_Query('showposts=1000');
+	?>
+	
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php if (  $case_posts->have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while (  $case_posts->have_posts() ) :  $case_posts->the_post(); ?>
+
+				<?php
+					/* Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'content', 'case_studies' );
+
+				?>
+
+			<?php endwhile; ?>
+
+			<?php intergalactic_paging_nav(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'content', 'none' ); ?>
+
+		<?php endif; ?>
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php
+
+}else{
 
 if (have_posts()) : while (have_posts()) : the_post(); ?>
 
@@ -25,7 +67,7 @@ if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 <?php
 
-
+}
 if  (!($post->post_title == 'About')){
 
 
