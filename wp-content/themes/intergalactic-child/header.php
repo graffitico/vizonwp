5f267d6b41e9cc5a4c77e81204a6ff01e274a8d3
graffitico <tech@graffitimedia.in>
2015-07-26 21:46:56 +0530

    Edited - sushant pls use

--- wp-content/themes/intergalactic-child/header.php
+++ wp-content/themes/intergalactic-child/header.php
@@ -16,7 +16,7 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<!-- <link rel="stylesheet" href="<?php // echo get_site_url() ?>/css/pages.css" > -->
 <?php wp_head(); ?>
 </head>
 
