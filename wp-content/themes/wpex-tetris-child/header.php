b31ae3951be5001edcafdc889d52ee2a31e40408
Sushant Sudarshan <sushant.sudarshan@myntra.com>
2015-08-26 23:42:20 +0530

    Header changes

--- wp-content/themes/wpex-tetris-child/header.php
+++ wp-content/themes/wpex-tetris-child/header.php
@@ -127,9 +127,9 @@
 								</a>
 							</li>
 							<li>
-								<a href='/products/convert/mobileweb-appreengagement/'>
+								<a href='/products/convert/mobileweb-appreengagement'>
 									<span class='icon icon-home_menuicons__webmobileapp'></span>
-									<span>Mobile Web-App Re-engagment</span>
+									<span>Mobile Web-App Re-engagement</span>
 								</a>
 							</li>
 							<li>
@@ -164,9 +164,10 @@
 							</li>
 						</ul>
 					</div>
-					<div class='contact-block product-subnav-block'>
+					<div class='contact-block no-border product-subnav-block'>
 						<p class='text-center' style='margin-top: 0'>For personalised marketing</p>
-						<input class='yellowbutton' style='padding: 10px 30px' type='button' value='GET IN TOUCH' />
+						<a href="/contactus">
+						<input class='yellowbutton' style='padding: 10px 30px' type='button' value='GET IN TOUCH' /></a>
 					</div>
 				</div>
 				<div class='subnav-block about-subnav'>
@@ -245,7 +246,25 @@
 						</ul>
 					</div>
 				</div>
-				<div class='resources-block resources-subnav-block'>
+				<div class='subnav-block resources-subnav'>
+					<div class='resources-block resources-subnav-block'>
+						<p class='text-center'>Catch some latest trends, listen to stories of customer delight, videos that unravel industry insights and everything else that's moving the marketing world all in one place.</p>
+						<!-- <ul class='subnav-list-control'>
+							<li>
+								<a href='/products/reach/webreach'>
+									<span class='icon icon-home_menuicons__webreach'></span>
+									<span>Web Reach</span>
+								</a>
+							</li>
+							<li>
+								<a href='/products/reach/appreengagement'>
+									<span class='icon icon-home_menuicons__appre_reach'></span>
+									<span>App Re-engagement</span>
+								</a>
+							</li>
+						</ul> -->
+					</div>
+					<div class='resources-block resources-subnav-block'>
 						<!-- <a href='/products/convert'><h3>Convert</h3></a>
 						<p class='text-center'>Case Studies</p> -->
 						<ul class='subnav-list-control'>
@@ -281,6 +300,12 @@
 							</li>
 						</ul>
 					</div>
+					<div class='contact-block no-border resources-subnav-block'>
+						<p class='text-center' style='margin-top: 0'>For personalised marketing</p>
+						<a href="/contactus">
+						<input class='yellowbutton' style='padding: 10px 30px' type='button' value='GET IN TOUCH' /></a>
+					</div>
+				</div>
 			</div>
 			<nav id="mobilenavigation-container">
 				<div class='navigation-trigger'>
@@ -320,7 +345,7 @@
 										<a href='/products/convert'>Convert Overview</a>
 									</li>
 									<li>
-										<a href='/products/convertgo'>Convert Go!</a>
+										<a href='/products/convert/convertgo'>Convert Go!</a>
 									</li>
 									<li>
 										<a href='/products/convert/appretargeting'>App Retargeting</a>
@@ -329,7 +354,7 @@
 										<a href='/products/convert/crosschannel'>Cross-Channel</a>
 									</li>
 									<li>
-										<a href='/products/convert/mobileweb-appengagement'>Mobile Web-App Re-engagement</a>
+										<a href='/products/convert/mobileweb-appreengagement'>Mobile Web-App Re-engagement</a>
 									</li>
 									<li>
 										<a href='/products/convert/facebookremarketing'>Facebook Remarketing</a>
