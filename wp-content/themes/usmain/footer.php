1e00dfe131bbb61073089a40af56082c71526452
shazadmaved <shazad26@gmail.com>
2015-09-19 20:21:53 +0530

    usmain theme

---
+++ wp-content/themes/usmain/footer.php
@@ -0,0 +1,5 @@
+    <script src="<?= get_template_directory_uri() ?>/js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?= get_template_directory_uri() ?>/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ wp-content/themes/usmain/index.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+<a href="<?= network_site_url() ?>?redirect=false">Back to main</a>
+<h1>hello</h1>
+
+
+<?php get_footer(); ?>
